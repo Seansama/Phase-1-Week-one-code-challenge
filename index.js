29740f9db9228360286ab6d29c49125db6a53b1e
Shaun <seansama@pop-os.localdomain>
2022-11-28 12:20:45 +0300

    Temporarily commented out code challenge 2

--- index.js
+++ index.js
@@ -2,25 +2,37 @@
 function studentGrade(){
     let grade = prompt("Please enter a grade between 0-100");//Prompts a user for input
     if(grade > 79){
-console.log("A");
+console.log("A");//Returns A if grade is above 79
     }
     else if(grade <= 79 && grade >= 60){
-console.log("B");
+console.log("B");//Returns B if grade is between 79- 60
     }
     else if(grade <= 59 && grade > 49) {
-        console.log("C");
+        console.log("C");//Returns C if grade is between 59 and 49
     }
-    else if(grade <= 49 && grade > 40) {
-        console.log("D");
+    else if(grade <= 49 && grade >= 40) {
+        console.log("D");//Returns D if grade is between 49 and 40
     }
     else if(grade < 40){
-        console.log("E");
+        console.log("E");//Return E if
     }
     else{
-        console.log("Please enter a valid grade");
+        console.log("Please enter a valid grade");//Asks the user to enter a valid grade if grade is not between 0 and 100
     }
 }
 studentGrade();
 //End of challenge 1
 
-//
+//Challenge 2: Speed detector
+/*
+speed = prompt("Please input speed");
+if (speed <=70) {
+    console.log("OK");
+}
+    else if(speed > 70){
+        let points = (speed - 70) / 5;
+        console.log('Your points are'${points})
+
+}
+*/
+
